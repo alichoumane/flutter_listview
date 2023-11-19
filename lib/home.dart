1044b935b0ef50b6f499c940328360253b80d4f7
alichoumane <ali.choumane@gmail.com>
2023-11-19 09:52:37 +0200

    part 1 - removed width

--- lib/home.dart
+++ lib/home.dart
@@ -11,37 +11,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    if(MediaQuery.of(context).orientation == Orientation.landscape)
-      screenWidth = screenWidth *0.8;
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Menu"),
         centerTitle: true,
       ),
-      body: ShowItems(width: screenWidth),
+      body: ShowItems(),
     );
   }
 }
 
 class ShowItems extends StatelessWidget {
-  final double width;
-  const ShowItems({super.key, required this.width});
+  const ShowItems({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print('width: $width');
     return ListView.builder(
       padding: const EdgeInsets.all(20.0),
         itemCount: items.length,
         itemBuilder: (context, index){
           return Column(children: [
             const SizedBox(height: 10),
-            SizedBox(width: width * 0.28,
-              child:Text(items[index].toString(), style: const TextStyle(fontSize: 18)),
-            ),
-          const SizedBox(height: 10)
+            Text(items[index].toString(), style: const TextStyle(fontSize: 18)),
+            const SizedBox(height: 10)
           ]);
         });
   }
